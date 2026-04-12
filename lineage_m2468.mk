92aed1b4835c9a71ee3253ecd34255b50c652c44
relimus <relimus@proton.me>
2026-04-12 09:03:01 +0800

    m2468: 配置 PRODUCT_GMS_CLIENTID_BASE

--- lineage_m2468.mk
+++ lineage_m2468.mk
@@ -19,3 +19,5 @@ PRODUCT_CHARACTERISTICS := device
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="qssi-user 15 AQ3A.250129.001 1737086932 release-keys" \
     BuildFingerprint="meizu/Meizu_21Note_CN/Meizu21Note:15/AQ3A.241229.001/1737086932:user/release-keys"
+
+PRODUCT_GMS_CLIENTID_BASE := android-meizu
