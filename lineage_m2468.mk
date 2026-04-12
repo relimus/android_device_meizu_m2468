15ebc3c8589c8f1405ee9ac47cd0549d97298615
relimus <relimus@proton.me>
2026-04-12 09:02:27 +0800

    m2468: 移除 PRODUCT_ENFORCE_ARTIFACT_PATH_REQUIREMENTS

--- lineage_m2468.mk
+++ lineage_m2468.mk
@@ -19,5 +19,3 @@ PRODUCT_CHARACTERISTICS := device
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="qssi-user 15 AQ3A.250129.001 1737086932 release-keys" \
     BuildFingerprint="meizu/Meizu_21Note_CN/Meizu21Note:15/AQ3A.241229.001/1737086932:user/release-keys"
-
-PRODUCT_ENFORCE_ARTIFACT_PATH_REQUIREMENTS :=
